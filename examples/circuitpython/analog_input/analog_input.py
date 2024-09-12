fa12e55be256237a72af8ff11261b0c770026724
Kong Wai Weng <waiweng@cytron.io>
2024-09-12 09:48:30 +0800

    Updated the reference voltage and edited some comments.

--- examples/circuitpython/analog_input/analog_input.py
+++ examples/circuitpython/analog_input/analog_input.py
@@ -16,17 +16,19 @@ import board
 import analogio
 
 # Supply voltage (mV).
-# This voltage should be around 3300 mV
-SUPPLY_VOLTAGE = 3300
+# This voltage should be around 3320 mV
+SUPPLY_VOLTAGE = 3320
  
 an0 = analogio.AnalogIn(board.AN0)
 an1 = analogio.AnalogIn(board.AN1)
  
 while True:
-    # Voltage Mode.
+    # Voltage Mode (Unit = mV).
+    # Make sure the AN-IN Mode switch position is "Voltage".
     print(an0.value * SUPPLY_VOLTAGE / 65536 * 16 / 5)
     
-    # Current Mode.
+    # Current Mode (Unit = uA).
+    # Make sure the AN-IN Mode switch position is "Current".
     print(an1.value * SUPPLY_VOLTAGE / 65536 * 16 / 5 / 248 * 1000)
     
     
